514effd0c4c32b7c08d8f2622338b7cac982c5c8
kunjut <v.dimnich@gmail.com>
2021-03-11 18:00:03 +0300

    Add empty constructor, add and setting few fields

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -5,10 +5,17 @@ import java.util.Objects;
 public class Person {
     private int id;
     private String name;
+    private String surname;
+    private String email;
 
-    public Person(int id, String name) {
+    public Person() {
+    }
+
+    public Person(int id, String name, String surname, String email) {
         this.id = id;
         this.name = name;
+        this.surname = surname;
+        this.email = email;
     }
 
     public int getId() {
@@ -25,6 +32,22 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
